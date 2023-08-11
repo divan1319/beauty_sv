7e7b53e45883c9649184acc902afa120666b4102
Daniel Iván López García <lopez.garcia1319@gmail.com>
2023-08-11 16:15:56 -0600

    Update PruebasView.vue

--- src/views/PruebasView.vue
+++ src/views/PruebasView.vue
@@ -2,29 +2,8 @@
     <div class="flex flex-col w-full ">
         <button @click="handlePruebas" class="w-32 bg-blue-800 p-2 rounded-lg hover:bg-blue-500 mx-auto mb-10">Probar</button>
         <button @click="handleRol" class="w-32 bg-blue-800 p-2 rounded-lg hover:bg-blue-500 text-lg text-white font-bold mx-auto">Agregar Rol</button>
+        <button @click="handleTech" class="w-32 bg-blue-800 p-2 rounded-lg hover:bg-blue-500 text-lg text-white font-bold mx-auto">Agregar Techs</button>
     </div>
-    <template id="my-template">
-  <swal-title>
-    Save changes to "Untitled 1" before closing?
-  </swal-title>
-  <swal-icon type="warning" color="red"></swal-icon>
-  <swal-button type="confirm">
-    Save As
-  </swal-button>
-  <swal-button type="cancel">
-    Cancel
-  </swal-button>
-  <swal-button type="deny">
-    Close without Saving
-  </swal-button>
-  <swal-param name="allowEscapeKey" value="false" />
-  <swal-param
-    name="customClass"
-    value='{ "popup": "my-popup" }' />
-  <swal-function-param
-    name="didOpen"
-    value="popup => console.log(popup)" />
-</template>
 </template>
 
 <script setup>
@@ -33,7 +12,7 @@ import clienteAxios from '../config/axios';
 import Swal from 'sweetalert2'
 const datos = reactive({
     name:'Salon de Belleza',
-    repo:'https://github.com/divan1319/backend-api',
+    repo:'https://github.com/divan1319/backend-api33',
     privacy:'publico',
     status:'desarrollo',
     limit_requets:6,
@@ -42,14 +21,18 @@ const datos = reactive({
 })
 
 const datosRol = reactive({
+    project_id:2,
+    user_id:3,
+    role_id:1
+})
+const datosTech = reactive({
     project_id:1,
-    user_id:2,
-    role_id:2
+    technology_id:[1,2]
 })
 const handlePruebas = async ()=>{
     await clienteAxios.post('/api/projects/create-project',datos,{
         headers:{
-            'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2OTE2NzY3MjQsImV4cCI6MTY5MTc2MzEyNCwibmJmIjoxNjkxNjc2NzI0LCJqdGkiOiJPazZobEJWV1k1bnR3bWV1Iiwic3ViIjoiMSIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.2m24mGI0BvFRs3SqbR7GTORMpTWNykLDsxk66-RbluA'
+            'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2OTE3NzQzNTUsImV4cCI6MTY5MTg2MDc1NSwibmJmIjoxNjkxNzc0MzU1LCJqdGkiOiJjcEV2Y2lXcm9PVmFZbzB2Iiwic3ViIjoiMSIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.MKia0KLpQAt_DK6oT6OcB3HgzVDDJayL866RiMBjrKk'
     }}).then(res => {
         
         console.log(res)
@@ -60,15 +43,44 @@ const handlePruebas = async ()=>{
 const handleRol = async () => {
     await clienteAxios.post('/api/projects/add-rol',datosRol,{
         headers:{
-            'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2OTE2NzY3MjQsImV4cCI6MTY5MTc2MzEyNCwibmJmIjoxNjkxNjc2NzI0LCJqdGkiOiJPazZobEJWV1k1bnR3bWV1Iiwic3ViIjoiMSIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.2m24mGI0BvFRs3SqbR7GTORMpTWNykLDsxk66-RbluA'
+            'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2OTE3NzQzNTUsImV4cCI6MTY5MTg2MDc1NSwibmJmIjoxNjkxNzc0MzU1LCJqdGkiOiJjcEV2Y2lXcm9PVmFZbzB2Iiwic3ViIjoiMSIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.MKia0KLpQAt_DK6oT6OcB3HgzVDDJayL866RiMBjrKk'
     }}).then(res => {
         Swal.fire({
-
-            template:'#my-template'
-            
+            icon:'success',
+            text:'Rol agregado correctamente',
+            showCancelButton:false,
+            confirmButtonText:'Aceptar',
         })
         console.log(res)
     }).catch(err => {
+        Swal.fire({
+            icon:'error',
+            text:'Hubo un error al asignar el rol',
+            showCancelButton:false,
+            confirmButtonText:'Aceptar'
+        })
+        console.log(err)
+    })
+}
+const handleTech = async () => {
+    await clienteAxios.post('/api/projects/add-rol2',datosTech,{
+        headers:{
+            'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMDAvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE2OTE3NzQzNTUsImV4cCI6MTY5MTg2MDc1NSwibmJmIjoxNjkxNzc0MzU1LCJqdGkiOiJjcEV2Y2lXcm9PVmFZbzB2Iiwic3ViIjoiMSIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.MKia0KLpQAt_DK6oT6OcB3HgzVDDJayL866RiMBjrKk'
+    }}).then(res => {
+        Swal.fire({
+            icon:'success',
+            text:'Techs agregados correctamente',
+            showCancelButton:false,
+            confirmButtonText:'Aceptar',
+        })
+        console.log(res)
+    }).catch(err => {
+        Swal.fire({
+            icon:'error',
+            text:'Hubo un error al asignar las techs',
+            showCancelButton:false,
+            confirmButtonText:'Aceptar'
+        })
         console.log(err)
     })
 }
